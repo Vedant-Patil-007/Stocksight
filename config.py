56b764c609919b18a0f2a25bc3e3f9f6f61caae4
Vedant-Patil-007 <90303790+Vedant-Patil-007@users.noreply.github.com>
2025-04-15 11:40:33 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+elasticsearch_host = "localhost"
+elasticsearch_port = 9200
+elasticsearch_user = "vedant"
+elasticsearch_password = "Dgvsp@2004007"
+
+consumer_key = "VARxl9dPR6qW1ygSm2zLibd4k"
+consumer_secret = "lNNPkeZbnEFJivHZSfVxCGu28IgfO2FrMZCq6KVlVtVlCuXIs8"
+access_token = "1701636521382797312-b9iO5IlakxVb0X7dLaYZwbssU1ClCd"
+access_token_secret = "BvlIImXmtrTUaUXJqyKKI7ibDyJe3drpyhBDylmTiDMUj"
+nltk_tokens_required = ("neuralink", "solar", "tesla", "@tesla", "#tesla", "tesla", "tsla", "#tsla", "elonmusk", "elon", "musk", "spacex", "starlink")
+nltk_min_tokens = 1
+nltk_tokens_ignored = ("win", "giveaway")
+twitter_feeds = ["@elonmusk", "@cnbc", "@benzinga", "@stockwits",
+                 "@Newsweek", "@WashingtonPost", "@breakoutstocks", "@bespokeinvest",
+                 "@WSJMarkets", "@stephanie_link", "@nytimesbusiness", "@IBDinvestors",
+                 "@WSJDealJournal", "@jimcramer", "@TheStalwart", "@TruthGundlach",
+                 "@Carl_C_Icahn", "@ReformedBroker", "@bespokeinvest", "@stlouisfed",
+                 "@muddywatersre", "@mcuban", "@AswathDamodaran", "@elerianm",
+                 "@MorganStanley", "@ianbremmer", "@GoldmanSachs", "@Wu_Tang_Finance",
+                 "@Schuldensuehner", "@NorthmanTrader", "@Frances_Coppola", "@bySamRo",
+                 "@BuzzFeed","@nytimes"]
